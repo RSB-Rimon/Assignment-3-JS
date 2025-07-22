af8072efa8f084f71765ed4447c7a1d74253d616
RSB-Rimon <rimonboydho7@gmail.com>
2025-07-22 23:24:40 +0600

    change something

--- Assignment-3.js
+++ Assignment-3.js
@@ -1,36 +1,22 @@
-// function isPrime(num){
-//     if (num<2){
-//         return false;
 
-//     }
-//     for(let i = 2; i<num; i++){
-//         if(num % i===0){
-//             return false;
-//         }
-//     }
-//     return true;
-// }
-// console.log(isPrime(3));
-// console.log(isPrime(5))
-// console.log(isPrime(10))
 
 // out put holo 3 hole true and 10 hole false hobe , prime songkha ja 1 dara bivajjo,
 
 // vowels problem 
 
-// function countVowels(charecter){
+function countVowels(charecter){
 
-// const vowels="aeiouAEIOU";
-// let count = 0;
-// for(let number of charecter){
-//     if(vowels.includes(number)){
-//         count++
-//     }
-// }
-// return count;
-// }
-// let result = countVowels("My name is Rimon Boydho. I am a student.")
-// console.log(result);
+const vowels="aeiouAEIOU";
+let count = 0;
+for(let number of charecter){
+    if(vowels.includes(number)){
+        count++
+    }
+}
+return count;
+}
+let result = countVowels("My name is Rimon Boydho. I am a student.")
+console.log(result);
 
 
 function fibonacci(number) {
